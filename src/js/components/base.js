0b3106baed752d24433ba114fa4a37d611445cea
Alex Cohen & Dirk Kelly <pair+acohen+dirkkelly@interexchange.io>
2014-10-08 17:03:31 -0400

    tooltipped with age

--- src/js/components/base.js
+++ src/js/components/base.js
@@ -17,3 +17,36 @@ var GenderIcon = React.createClass({
     )
   }
 });
+
+var TooltippedYearCalculator = React.createClass({
+  componentDidMount: function() {
+    $(this.getDOMNode()).tooltip();
+  },
+
+  render: function() {
+    var to = Date.today(this.props.to),
+        from = Date.parse(this.props.from),
+        period = new TimePeriod(to, from);
+
+    return (
+      <span data-toggle="tooltip" data-placement="top" title={this.props.tooltipTitle}>
+        {period.years}
+      </span>
+    )
+  }
+});
+
+var TooltippedIconSpan = React.createClass({
+  componentDidMount: function() {
+    $(this.getDOMNode()).tooltip();
+  },
+
+  render: function() {
+    return (
+      <span data-toggle="tooltip" data-placement="top" title={this.props.tooltipTitle}>
+        <i className={this.props.iconClass}></i>
+        {this.props.spanContent}
+      </span>
+    )
+  }
+});
