4b55336f9da2b5bfd6fce4f47ff7dc44ce7303dc
Nathan Harper <pair+nathanharper@interexchange.io>
2014-10-29 16:58:35 -0400

    include DraftJobOffers with OfferedParticipantGroup

--- src/jsx/components/offered-participant-group-panel.jsx
+++ src/jsx/components/offered-participant-group-panel.jsx
@@ -1,7 +1,6 @@
 var OfferedParticipantGroupPanels = React.createClass({
   getInitialState: function() {
     return {
-      employer: null,
       offeredParticipantGroups: null
     };
   },
@@ -10,7 +9,6 @@ var OfferedParticipantGroupPanels = React.createClass({
     $.get(this.props.source, function(data) {
       if (this.isMounted()) {
         this.setState({
-          employer: data.employer,
           offeredParticipantGroups: data.offered_participant_groups
         });
       }
@@ -19,12 +17,12 @@ var OfferedParticipantGroupPanels = React.createClass({
 
   render: function() {
     if (this.isMounted()) {
-      var employer = this.state.employer,
+      var employerId = this.props.employerId,
           offeredParticipantGroupPanels = this.state.offeredParticipantGroups.map(function (offeredParticipantGroup) {
-            return (
-              <OfferedParticipantGroupPanel key={offeredParticipantGroup.id} data={offeredParticipantGroup} employer={employer} />
-            );
-          });
+        return (
+          <OfferedParticipantGroupPanel key={offeredParticipantGroup.id} data={offeredParticipantGroup} employerId={employerId} />
+        );
+      });
 
       return (
         <div id="participant-group-panels">
@@ -39,136 +37,67 @@ var OfferedParticipantGroupPanels = React.createClass({
 
 var OfferedParticipantGroupPanel = React.createClass({
   getInitialState: function() {
-    return {
-      isOffering: false,
-      draftJobOfferValid: false
-    };
+    return { sending: false, puttingOnReview: false };
   },
 
-  toggleIsOffering: function(event) {
-    this.setState({ isOffering: !this.state.isOffering });
-  },
-
-  toggleDraftJobOfferValid: function () {
-    this.setState({draftJobOfferValid: !this.state.draftJobOfferValid});
-  },
-
-  handleSubmit: function(event) {
-    event.preventDefault();
-
-    var node = this.getDOMNode(),
-        form = $(event.target),
-        data = form.serialize();
-
-    $.ajax({
-      url: "/offered_participant_groups/" + this.props.key + "/confirm.json",
-      type: "POST",
-      data: data,
-      success: function(data) {
-        React.unmountComponentAtNode(node);
-        $(node).remove();
-      },
-      error: function(data) {
-        console.log(data);
-      }
-    });
+  componentWillMount: function() {
+    this.props.onReviewExpiresOn = Date.today().add(3).days().toString('yyyy-MM-dd');
   },
 
   render: function() {
-    return (
-      <form className="panel panel-default participant-group-panel form-horizontal" role="form" onSubmit={this.handleSubmit}>
-        <OfferedParticipantGroupPanelListGroup data={this.props.data} isOffering={this.state.isOffering} draftJobOfferValid={this.state.draftJobOfferValid} toggleDraftJobOfferValid={this.toggleDraftJobOfferValid}  />
-        <OfferedParticipantGroupPanelFooter data={this.props.data} draftJobOfferValid={this.state.draftJobOfferValid} isOffering={this.state.isOffering} toggleIsOffering={this.toggleIsOffering} />
-      </form>
-    )
-  }
-});
-
-var OfferedParticipantGroupPanelListGroup = React.createClass({
-  getInitialState: function () {
-    var participantCount = this.props.data.participants.length,
-        participantValidationStatuses = [];
-
-    for (var i=0; i<participantCount; i++) {
-      participantValidationStatuses.push(false);
-    }
-
-    return { "participantValidationStatuses": participantValidationStatuses };
-  },
-
-  isFormValid: function () {
-    return this.state.participantValidationStatuses.reduce(function (prev, curr) {
-      return prev && curr;
-    });
-  },
-
-  updateNodeStatus: function (nodeNumber, isValid) {
-    var participantValidationStatuses = this.state.participantValidationStatuses,
-        oldStatus = this.isFormValid();
-
-    participantValidationStatuses[nodeNumber] = isValid;
-    this.setState({"participantValidationStatuses": participantValidationStatuses});
-
-    if (oldStatus !== this.isFormValid()) {
-      this.props.toggleDraftJobOfferValid();
-    }
-  },
-
-  render: function() {
-    var isOffering = this.props.isOffering,
-        nodeNumber = 0,
-        updateNodeStatus = this.updateNodeStatus,
-        draftJobOfferValid = this.props.draftJobOfferValid,
+    var actionRow,
+        draftJobOffers = this.props.data.draft_job_offers,
         participantNodes = this.props.data.participants.map(function (participant) {
-          if (!isOffering) {
-            return (
-              <ParticipantGroupParticipant key={participant.id} data={participant} />
-            )
-          } else {
-            return (
-              <ParticipantGroupParticipantOffering draftJobOfferValid={draftJobOfferValid} updateNodeStatus={updateNodeStatus} nodeNumber={nodeNumber++} key={participant.id} data={participant} />
-            )
-          }
+          var draftJobOffer = draftJobOffers.filter(function (draft_job_offer) {
+            return draft_job_offer.participant_id == participant.id;
+          })[0];
+          return (
+            <OfferedParticipantGroupParticipant key={participant.id} data={participant} draftJobOffer={draftJobOffer} />
+          )
         });
 
+    actionRow = <div className="row">
+      <div className="col-xs-3 col-sm-3">
+        <div className="panel-title pull-left">{this.props.data.name}</div>
+      </div>
+    </div>
+
     return (
-      <div>
-        {participantNodes}
+      <div className="panel panel-default participant-group-panel">
+        <div className="list-group">
+          {participantNodes}
+        </div>
+        <div className="panel-footer clearfix">
+          {actionRow}
+        </div>
       </div>
     )
   }
 });
 
-var OfferedParticipantGroupPanelFooter = React.createClass({
-  propogateToggleIsOffering: function () {
-    this.props.toggleIsOffering(this);
+var OfferedParticipantGroupParticipant = React.createClass({
+  getInitialState: function () {
+    return {};
   },
 
-  render: function() {
-    var isOffering = this.props.isOffering,
-        propogateToggleIsOffering = this.props.toggleIsOffering,
-        draftJobOfferValid = this.props.draftJobOfferValid,
-        legalize = (function (participant) {
-          if (isOffering) {
-            return (
-              <div className="row">
-                <hr/>
-                <small className="col-xs-12 text-right">
-                  By clicking offer I agree that the information entered is true and accurate to the best of my knowledge and that I will contact InterExchange if any information changes.
-                </small>
-              </div>
-            )
-          }
-        })();
-
+  render: function () {
     return (
-      <div className="panel-footer clearfix">
-        <div className="row">
-          <div className="col-xs-3 col-sm-3">
-            <div className="panel-title pull-left">{this.props.data.name}</div>
+      <div className="list-group-item list-group-item-participant" data-participant-name={this.props.data.name}>
+        <div className="media">
+          <img className="media-object img-circle img-thumbnail pull-left" src={this.props.data.photo_url} alt="{this.props.data.name}" />
+          <div className="media-body">
+            <div className="row">
+              <div className="col-xs-12">
+                <h4 className="media-heading">{this.props.data.name}</h4>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-xs-12">
+                <div>{this.props.draftJobOffer.position}</div>
+              </div>
+            </div>
           </div>
         </div>
-        {legalize}
       </div>
     )
   }
