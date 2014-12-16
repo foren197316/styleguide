4ef6f413e543833a124fb03e8aa13fe68e0e39a9
Dirk Kelly <iex-eng+dirkkelly@googlegroups.com>
2014-12-16 13:28:01 +0800

    returned signed filter to offered

--- src/jsx/components/offered-participant-groups-index.jsx
+++ src/jsx/components/offered-participant-groups-index.jsx
@@ -191,6 +191,7 @@ var OfferedParticipantGroupsIndex = React.createClass({
         <div className="col-md-3">
           <SearchFilter title="offered_names" searchOn="participant_names" options={this.props.offeredParticipantGroups} dataLink={offeredParticipantGroupsLink} />
           <CheckBoxFilter title="Program" options={this.props.programs} dataLink={programsLink} />
+          <CheckBoxFilter title="Participant Agreement" options={this.props.participantSigned} dataLink={participantSignedLink} />
           <CheckBoxFilter title="Offer Sent" options={this.props.offerSent} dataLink={offerSentLink} />
           <CheckBoxFilter title="Coordinator" options={this.props.staffs} dataLink={staffsLink} allUnselectedLink={allStaffsUnselectedLink} />
           <CheckBoxFilter title="Employer" options={this.props.employers} dataLink={employersLink} />
