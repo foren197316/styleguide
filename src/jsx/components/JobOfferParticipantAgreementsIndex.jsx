0af0d47e11048e24d5b3b04d99575a86f64d3c1d
Nathan Harper <iex-eng+nathanharper@googlegroups.com>
2015-01-12 11:19:37 -0500

    Search Email/UUID

--- src/jsx/components/JobOfferParticipantAgreementsIndex.jsx
+++ src/jsx/components/JobOfferParticipantAgreementsIndex.jsx
@@ -5,7 +5,7 @@ var JobOfferParticipantAgreementsIndex = React.createClass({
     return (
       <div className="row">
         <div className="col-md-3">
-          <SearchFilter title="Search" searchOn={[["job_offer", "participant", "name"]]} actions={JobOfferParticipantAgreementActions} />
+          <SearchFilter title="Search" searchOn={[["job_offer", "participant", "name"], ["job_offer", "participant", "email"], ["job_offer", "participant", "uuid"]]} actions={JobOfferParticipantAgreementActions} />
           <CheckBoxFilter title="Program" store={ProgramStore} actions={ProgramActions} />
           <BooleanFilter title="FileMaker" label="Not in FileMaker" action={JobOfferParticipantAgreementStore.toggleNotInFileMaker} />
         </div>
