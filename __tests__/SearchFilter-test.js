8b3eb3a2f8f7834f3719137b08f8a23985c14042
Nathan Harper <iex-eng+nathanharper@googlegroups.com>
2015-01-16 16:27:06 -0500

    unit test for SearchFilter

--- __tests__/SearchFilter-test.js
+++ __tests__/SearchFilter-test.js
@@ -5,17 +5,31 @@ describe('SearchFilter', function () {
     var React = require('../node_modules/react/addons');
     var SearchFilter = require('../src/js/components/SearchFilter.js');
     var TestUtils = React.addons.TestUtils;
+
+    var title = "Search";
+    var value = "Brenda";
+    var searchOn = "participant_names";
     var actions = {
-      search: function(){},
-      resetSearch: function(){}
+      search: jest.genMockFunction(),
+      resetSearch: jest.genMockFunction()
     };
 
     var searchFilter = TestUtils.renderIntoDocument(
       SearchFilter({
-        title: "Search",
-        searchOn: "participant_names",
+        title: title,
+        searchOn: searchOn,
         actions: actions
       })
     );
+
+    var input = TestUtils.findRenderedDOMComponentWithTag(searchFilter, 'input');
+
+    TestUtils.Simulate.change(input, {target: {value: value}});
+
+    expect(actions.search).toBeCalledWith(title, value, searchOn);
+
+    TestUtils.Simulate.change(input, {target: {value: ''}});
+
+    expect(actions.resetSearch).toBeCalledWith(title);
   });
 });
