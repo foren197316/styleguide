85d958146057849ee82a8e82fd9232f417abc831
Nathan Harper <iex-eng+nathanharper@googlegroups.com>
2015-02-24 09:47:45 -0500

    error reporting in dev

--- src/js/main.js
+++ src/js/main.js
@@ -5,6 +5,9 @@ var Reflux = require('reflux');
 var $ = require('jquery');
 var Base64 = require('./base64');
 var moment = require('moment');
+var React = require('react/addons');
+var withRootNode = require('./root-node');
+
 
 moment.locale('en', {
   relativeTime: {
@@ -37,22 +40,19 @@ if (__DEV__) {
   global.Intercom = function (action, name, data) {
     console.log('Intercom', action, name, data);
   };
-} else {
-  var React = require('react/addons');
-  var withRootNode = require('./root-node');
-
-  global.onerror = function (message) {
-    withRootNode(function (rootNode) {
-      React.render(
-        React.DOM.div({className: 'alert alert-danger'},
-          React.DOM.strong(null, 'An error occurred: '), message
-        ),
-        rootNode
-      );
-    });
-    return true;
-  };
 }
+
+global.onerror = function (message) {
+  withRootNode(function (rootNode) {
+    React.render(
+      React.DOM.div({className: 'alert alert-danger'},
+        React.DOM.strong(null, 'An error occurred: '), message
+      ),
+      rootNode
+    );
+  });
+  return false;
+};
 
 String.prototype.capitaliseWord = function () {
   return this.charAt(0).toUpperCase() + this.slice(1);
