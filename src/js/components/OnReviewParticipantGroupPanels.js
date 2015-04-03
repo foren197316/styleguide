6b6c3aeeb3be1f2f0a7f46a24b309349a8af873d
Nathan Harper <iex-eng+nathanharper@googlegroups.com>
2015-04-03 13:44:58 -0400

    on review global list pagination

--- src/js/components/OnReviewParticipantGroupPanels.js
+++ src/js/components/OnReviewParticipantGroupPanels.js
@@ -3,25 +3,33 @@
 
 let React = require('react/addons');
 let Reflux = require('reflux');
+let ReloadingComponent = React.createFactory(require('./ReloadingComponent'));
+let Pagination = React.createFactory(require('./Pagination'));
 let OnReviewParticipantGroupPanel = React.createFactory(require('./OnReviewParticipantGroupPanel'));
 let OnReviewParticipantGroupStore = require('../stores/OnReviewParticipantGroupStore');
 let EmployerStore = require('../stores/EmployerStore');
 let PositionStore = require('../stores/PositionStore');
+let MetaStore = require('../stores/MetaStore');
 let { RenderLoadedMixin } = require('../mixins');
 let { OnReviewParticipantGroupActions, PositionActions, loadFromOnReviewParticipantGroups } = require('../actions');
-let { getQuery } = require('../query');
+let { getQuery, getCurrentPage } = require('../query');
+let { div, a } = React.DOM;
 
 let OnReviewParticipantGroupPanels = React.createClass({
   displayName: 'OnReviewParticipantGroupPanels',
   mixins: [
+    React.addons.LinkedStateMixin,
     Reflux.connect(OnReviewParticipantGroupStore, 'onReviewParticipantGroups'),
     Reflux.connect(EmployerStore, 'employers'),
     Reflux.connect(PositionStore, 'positions'),
-    RenderLoadedMixin('onReviewParticipantGroups', 'employers', 'positions')
+    Reflux.connect(MetaStore, 'meta'),
+    RenderLoadedMixin('onReviewParticipantGroups', 'employers', 'positions', 'meta')
   ],
 
   getInitialState () {
-    return { groups: [] };
+    return {
+      formSending: false
+    };
   },
 
   componentDidMount () {
@@ -30,14 +38,40 @@ let OnReviewParticipantGroupPanels = React.createClass({
   },
 
   renderLoaded () {
-    let { positions } = this.state;
+    let { positions, employers, meta, onReviewParticipantGroups } = this.state;
+    let formSending = this.linkState('formSending');
+    let recordName = 'Participant';
+    let anchor = 'searchTop';
+    let page = getCurrentPage();
+    let { pageCount, recordCount } = meta;
+    let callbacks = [loadFromOnReviewParticipantGroups];
 
     return (
-      React.DOM.div({id: 'participant-group-panels'},
-        this.state.onReviewParticipantGroups.map((onReviewParticipantGroup, key) => {
-          let employer = this.state.employers.findById(onReviewParticipantGroup.employer_id);
-          return OnReviewParticipantGroupPanel({key, onReviewParticipantGroup, employer, positions});
-        })
+      div({className: 'row'},
+        div({className: 'col-md-3'}),
+        div({className: 'col-md-9'},
+          div({id: 'participant-group-panels'},
+            a({ name: anchor }),
+            ReloadingComponent({ loadingLink: formSending },
+              div({className: 'row'},
+                div({className: 'col-md-12'},
+                  Pagination({ pageCount, recordCount, page, actions: OnReviewParticipantGroupActions, formSending, recordName, callbacks })
+                )
+              ),
+              onReviewParticipantGroups.map((onReviewParticipantGroup, key) => {
+                let employer = employers.findById(onReviewParticipantGroup.employer_id);
+                if (employer) {
+                  return OnReviewParticipantGroupPanel({key, onReviewParticipantGroup, employer, positions});
+                }
+              }),
+              div({className: 'row'},
+                div({className: 'col-md-12'},
+                  Pagination({ pageCount, recordCount, page, anchor, actions: OnReviewParticipantGroupActions, formSending, recordName, callbacks })
+                )
+              )
+            )
+          )
+        )
       )
     );
   }
