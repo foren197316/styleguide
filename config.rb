41886ecbc232b6315b23f422810cb8e4cb4e1645
Dirk Kelly <iex-eng+dirkkelly@googlegroups.com>
2015-04-30 09:42:34 -0400

    Layouts in index

--- config.rb
+++ config.rb
@@ -8,7 +8,8 @@ set :images_dir, 'images'
 set :url_root, ENV.fetch("URL_ROOT")
 set :analytics_key, ENV.fetch("ANALYTICS_KEY")
 
-page "index.html", :layout => :article_layout
+page "index.html", layout: :index_layout
+page "examples/layouts/article.html", layout: :article_layout
 
 configure :build do
 end
